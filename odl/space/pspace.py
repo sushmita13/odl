a5071733ae4c739c6cde95055323938a3ee35d55
Holger Kohr <h.kohr@cwi.nl>
2017-01-24 18:48:08 +0100

    MAINT: fixes to deformation operator and others

--- odl/space/pspace.py
+++ odl/space/pspace.py
@@ -26,7 +26,7 @@ from odl.util import is_real_dtype
 from odl.util.ufuncs import ProductSpaceUfuncs
 
 
-__all__ = ('ProductSpace', 'ProductSpaceElement')
+__all__ = ('ProductSpace',)
 
 
 class ProductSpace(LinearSpace):
